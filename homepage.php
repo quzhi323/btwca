b9c4442111ea4be73b33b39a3254b3e304c641f9
quzhi323 <quzhi323@gmail.com>
2017-12-23 18:05:05 -0500

    modify

--- homepage.php
+++ homepage.php
@@ -495,7 +495,7 @@
                 button.setAttribute("data-target","#"+record_list[i].id);
                 button.innerHTML="查看详情";
 
-                var fun_state = "createModal('"+record_list[i].id+"','"+record_list[i].datetime+"','"+record_list[i].phone+"','"+record_list[i].wechat+"','"+record_list[i].descpt+"','"+dept_info+"','"+"dest_info+"')";
+                var fun_state = "createModal('"+record_list[i].id+"','"+record_list[i].datetime+"','"+record_list[i].phone+"','"+record_list[i].wechat+"','"+record_list[i].descpt+"','"+dept_info+"','"+dest_info+"')";
                 button.setAttribute("onclick",fun_state);
                 tbody.append(tr);
 
